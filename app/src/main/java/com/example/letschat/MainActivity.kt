380b42edce28d901aaa0abe0826f8477616c1836
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-17 08:17:16 +0300

    feat: Log on create and on view created

--- app/src/main/java/com/example/letschat/MainActivity.kt
+++ app/src/main/java/com/example/letschat/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.letschat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.example.letschat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        Log.i("Main", "Activity created")
     }
 }
