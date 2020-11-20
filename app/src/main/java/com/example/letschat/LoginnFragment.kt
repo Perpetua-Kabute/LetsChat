92bbdcaa08b88d761a45093eec813230ab2fcea2
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-20 14:00:00 +0300

    feat: Add login fragment

--- app/src/main/java/com/example/letschat/LoginnFragment.kt
+++ app/src/main/java/com/example/letschat/LoginnFragment.kt
@@ -32,7 +32,7 @@ class LoginnFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loginn, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     companion object {
