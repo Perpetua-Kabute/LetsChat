14732c98c80e74b0f42028bb5fe59452b6a237b0
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-20 16:24:03 +0300

    fix: fix login fragment name

--- app/src/main/java/com/example/letschat/LoginFragment.kt
+++ app/src/main/java/com/example/letschat/LoginFragment.kt
@@ -13,13 +13,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginnFragment.newInstance] factory method to
+ * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginnFragment : Fragment() {
+class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,7 +48,7 @@ class LoginnFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-                LoginnFragment().apply {
+                LoginFragment().apply {
                     arguments = Bundle().apply {
                         putString(ARG_PARAM1, param1)
                         putString(ARG_PARAM2, param2)
