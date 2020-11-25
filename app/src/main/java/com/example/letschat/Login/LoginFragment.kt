41c6e4301df566a26a9c49264a58675ad81a68d9
Perpetua-Kabute <kabuteperpetua@gmail.com>
2020-11-26 00:42:20 +0300

    feat: Add user to database

--- app/src/main/java/com/example/letschat/Login/LoginFragment.kt
+++ app/src/main/java/com/example/letschat/Login/LoginFragment.kt
@@ -1,20 +1,25 @@
 package com.example.letschat.Login
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
+import com.example.letschat.MainActivity
 import com.example.letschat.R
 import com.example.letschat.database.Device
+import com.example.letschat.database.DeviceDatabaseDao
 import com.example.letschat.database.LetschatDatabase
 import com.example.letschat.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
+    private lateinit var databaseDao: DeviceDatabaseDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +30,7 @@ class LoginFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login,container, false)
 //        //get the application
-//        val application = requireNotNull(this.activity).application
+        val application = requireNotNull(this.activity).application
 //
 //        //reference to datasource
 //        val dataSource = LetschatDatabase.getInstance(application).deviceDatabaseDao
@@ -39,12 +44,13 @@ class LoginFragment : Fragment() {
 //        binding.setLifecycleOwner(thiss)
         val deviceName = binding.deviceName.text.toString()
         binding.signIn.setOnClickListener {
-            if(deviceName.isEmpty()){
+            if(TextUtils.isEmpty(deviceName)){
                 Log.i("LoginFragment", "Device name empty")
-
+                Toast.makeText(activity, "Enter the name of your device", Toast.LENGTH_SHORT).show()
             }
-            val userDevice=  Device(deviceName = deviceName)
-
+            val userDevice= Device(deviceName = deviceName)
+            databaseDao.insert(userDevice)
+            
 
         }
 
